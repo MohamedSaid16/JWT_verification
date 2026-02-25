2d4c4947921b14ddbd19e3d40e61fe04483f9d20
Mohamed Said <medsaidghoulam@gmail.com>
2026-02-25 17:00:01 +0100

    Update app.ts with new content - 2026-02-25 15:58:56

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,20 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes";
+import { errorHandler, notFoundHandler } from "./middlewares/error.middleware";
+import logger from "./utils/logger";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+
+app.use((req, _res, next) => {
+  logger.info(`${req.method} ${req.originalUrl}`);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -12,5 +22,10 @@ app.get("/", (req, res) => {
     message: "University API Running",
   });
 });
+
+app.use("/api/v1/auth", authRoutes);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 export default app;
